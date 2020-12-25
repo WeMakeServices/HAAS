8fc82f07c6b4339a49bdaed3438f2f120a501121
Christian Scillitoe <christian.scillitoe@gmail.com>
2020-12-25 05:08:58 +0000

    Functional HAAS

--- HAAS.py
+++ HAAS.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request
 from flask_limiter import Limiter
 from flask_cors import CORS, cross_origin
@@ -12,11 +11,37 @@ limiter = Limiter(
 )
 
 
-@app.route("/sample", methods=["GET"])
+@app.route("/status_code_please/<status_code>", methods=["GET"])
 @cross_origin(support_credentials=True)
-@limiter.limit("1 per day")
-def sample():
-    return "Hello World!", 200
+@limiter.limit("30 per 7 days")
+def status_code_please(status_code):
+    try:
+        status_code = validate_and_return_properly_parsed_status_code_if_parsing_was_successful_otherwise_raise_one_of_multiple_custom_exceptions_based_on_exception_criteria(
+            status_code
+        )
+    except:
+        return "Bad request", 400
+    return str(status_code), status_code
+
+
+def validate_and_return_properly_parsed_status_code_if_parsing_was_successful_otherwise_raise_one_of_multiple_custom_exceptions_based_on_exception_criteria(
+    status_code: str,
+):
+    status_code = int(status_code)
+    if status_code <= 0:
+        raise StatusCodeNonPositive
+    if status_code > 2147483647:
+        raise StatusCodeTooLarge
+
+    return status_code
+
+
+class StatusCodeNonPositive(Exception):
+    pass
+
+
+class StatusCodeTooLarge(Exception):
+    pass
 
 
 if __name__ == "__main__":
